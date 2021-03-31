1a15deac07fced4e22bae0c69616d656ea87fb33
oleksiisavin <50924031+oleksiisavin@users.noreply.github.com>
2021-03-31 21:47:14 +0300

    Updated config.js

--- config.js
+++ config.js
@@ -1,8 +1,10 @@
-  (module.exports = {
+module.exports = {
   endpoint: 'https://api.github.com',
   token: '#########################',
   platform: 'github',
+  onboardingConfig: {
+    extends: ['config:base'],
+  }  
   repositories: ['###/###'],
   logLevel: 'debug',
  };
-)
